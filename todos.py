c72481d9a96617744b85c17e972da7b3c5c3817e
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-29 20:47:20 +0200

    small refactor

---
+++ todos.py
@@ -0,0 +1,5 @@
+# TODO 1: Make simple GUI
+# TODO 2: Clean up read/write functionality (pandas, csv)
+# TODO 3: Implement data visualization with matplotlib
+# TODO 4: Implement language selection (PL, ENG, ES)
+
