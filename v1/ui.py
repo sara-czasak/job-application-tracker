5cf4eff7accb4e25075647e1749bb6b912c441c3
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-30 10:45:52 +0200

    working on treeview

--- v1/ui.py
+++ v1/ui.py
@@ -30,6 +30,21 @@ search_button = ttk.Button(frm, text="SEARCH")
 search_button.grid(column=2, row=2, padx=2, pady=2)
 
 
+# This will go with the show all button!
+tree = ttk.Treeview(frm, columns=['job_name', 'job_type', 'public_transport', 'job_address', 'date_applied', 'job_status'], show='headings')
+tree.heading('job_name', text='Job Name')
+tree.column('job_name', width=100)
+tree.heading('job_type', text='Job Type')
+tree.column('job_type', width=100)
+tree.heading('public_transport', text='Public Transport')
+tree.column('public_transport', width=100)
+tree.heading('job_address', text='Job Address')
+tree.column('job_address', width=100)
+tree.heading('date_applied', text='Date Applied')
+tree.column('date_applied', width=100)
+tree.heading('job_status', text='Job Status')
+tree.column('job_status', width=100)
+tree.grid(column=0, row=3, columnspan=3, padx=2, pady=2)
 
 
 
