ef5a5e757c97d398ad58dcf88dd3b5c0e538b812
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-30 16:37:24 +0200

    remove unneeded function

--- v1/button_func.py
+++ v1/button_func.py
@@ -1,17 +1,4 @@
-import pandas as pd
 import tkinter as tk
-import tkinter.ttk as ttk
-
-
-# SHOW ALL BUTTON
-def show_all(data, tree):
-    if data.empty:
-        return 'No job data added yet'
-    else:
-        rows = data.iterrows()
-        for row in rows:
-            tree.insert('', tk.END, values=list(row[1].values))
-        return None
 
 
 def layout_hide_show(button_id, context):
